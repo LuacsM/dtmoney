f7e7d3b5b955e8e8f34a54eb09a774b8cf565953
Lucas Maia <lucas.maia.mn@gmail.com>
2022-08-09 10:00:36 -0300

    form-modal inserido

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { Dashboard } from "./componenents/Dashboard";
+import Modal from 'react-modal';
 import { Header } from "./componenents/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./componenents/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+     setIsNewTransactionModalOpen(false);   
+    }
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <Dashboard/>
       <GlobalStyle/>
     </>
